ca05bd31e7e8c091a4037dffc46e8f9c4c2c442e
Savage9193 <tyagishahvez551@gmail.com>
2024-05-13 02:52:22 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from "react";
+import "./App.css"
 
-function App() {
+const App = () => {
+  const [weight, setWeight] = useState(120);
+  const [height, setHeight] = useState(70);
+
+  function onWeightChange(event) {
+    setWeight(event.target.value);
+  }
+  function onHeightChange(event) {
+    setHeight(event.target.value);
+  }
+  const output = useMemo(() => {
+    const CalculateHeight = height / 100;
+    return (weight / (CalculateHeight * CalculateHeight)).toFixed(1);
+  }, [weight, height]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main>
+        <h1>BMI CALCULATOR</h1>
+        <div className="input-section">
+          <p className="slider-output">Weight: {weight}kg</p>
+          <input
+            className="input-slider"
+            type="range"
+            step={1}
+            min={30}
+            max={150}
+            onChange={onWeightChange}
+          />
+          <p className="slider-output">Height: {height}cm</p>
+          <input
+            className="input-slider"
+            type="range"
+            step={1}
+            min={80}
+            max={300}
+            onChange={onHeightChange}
+          />
+        </div>
+        <div className="output-section">
+          <p>Your BMI is </p>
+          <p className="output">{output}</p>
+        </div>
+      </main>
+      <p className="myname">Made by: Shahvez Tyagi:&#10084;</p>
     </div>
   );
-}
+};
 
 export default App;
